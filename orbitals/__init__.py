5e2d070d71aea7557505035d633026d240789fdb
verabaranova2000 <126353652+verabaranova2000@users.noreply.github.com>
2025-12-10 16:35:42 +0000

    Дополнен __init__.py

--- orbitals/__init__.py
+++ orbitals/__init__.py
@@ -1,1 +1,3 @@
 from .grasp_reader import read_occupations_from_grasp_sum, read_rwfn_plot
+from .compute_f import safe_sinc, compute_fq
+from .compute_rho import compute_rho_orbital, normalize_rho_1e, build_rho_total, check_electrons, build_rho_core
