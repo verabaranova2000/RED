7bcbbaf8cf78705c736ceb33777a92d5c93c1e62
verabaranova2000 <126353652+verabaranova2000@users.noreply.github.com>
2026-03-10 06:55:23 +0000

    Исправлен refinement/__init__,py

--- refinement/__init__.py
+++ refinement/__init__.py
@@ -1,18 +1,16 @@
 from .session import RefinementSession
 
-from .param_utils import (
-    prepare_params,
-    deepcopy_params,
-    params_for_next,
-    val_delta_percent,
-    relative_change
-)
+#from .param_utils import (
+#    prepare_params,
+#    deepcopy_params,
+#    params_for_next,
+#    val_delta_percent,
+#    relative_change
+#)
 
-__all__ = [
-    "RefinementSession",
-    "prepare_params",
-    "deepcopy_params",
-    "params_for_next",
-    "val_delta_percent",
-    "relative_change"
-    ]
+__all__ = ["RefinementSession"]
+#    "prepare_params",
+#    "deepcopy_params",
+#    "params_for_next",
+#    "val_delta_percent",
+#    "relative_change"
